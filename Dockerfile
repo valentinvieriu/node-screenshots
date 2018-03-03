938852fe8c1823b119670aca64f747a408366503
Valentin Vieriu <valentin.vieriu@gmail.com>
2018-03-03 16:55:32 +0100

    working with pupeteer

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,26 @@
-FROM node:9.5.0 as build
+FROM node:9.7.1
+
+# See https://crbug.com/795759
+RUN apt-get update && apt-get install -yq libgconf-2-4
+
+# Install latest chrome dev package and fonts to support major charsets (Chinese, Japanese, Arabic, Hebrew, Thai and a few others)
+# Note: this installs the necessary libs to make the bundled version of Chromium that Puppeteer
+# installs, work.
+RUN apt-get update && apt-get install -y wget --no-install-recommends \
+  && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+  && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+  && apt-get update \
+  && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont \
+  --no-install-recommends \
+  && rm -rf /var/lib/apt/lists/* \
+  && apt-get purge --auto-remove \
+  && rm -rf /src/*.deb
+
+# Uncomment to skip the chromium download when installing puppeteer. If you do,
+# you'll need to launch puppeteer with:
+#     browser.launch({executablePath: 'google-chrome-unstable'})
+# ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+
 # set our node environment, either development or production
 # defaults to production, compose overrides this to development on build and run
 ARG NODE_ENV=production
@@ -8,17 +30,15 @@ WORKDIR /usr/src
 COPY package.json /usr/src
 COPY package-lock.json /usr/src
 RUN npm set progress=false && \
-npm config set depth 0 && \
-npm install && \
-npm cache clean --force && \
-rm -rf /tmp/*
+  npm config set depth 0 && \
+  npm install && \
+  npm cache clean --force && \
+  rm -rf /tmp/*
 
-FROM node:9.5.0-slim
 # copy in our source code last, as it changes the most
 WORKDIR /usr/src/app
 ENV PATH /usr/src/node_modules/.bin:$PATH
 
-COPY --from=build /usr/src /usr/src
 COPY . /usr/src/app
 
 # set our node environment, either development or production
@@ -32,4 +52,15 @@ ENV NODE_ENV $NODE_ENV
 ARG PORT=80
 ENV PORT $PORT
 EXPOSE $PORT 5858 9229
-CMD [ "node","--harmony", "app.js" ]
+
+# # # Add puppeteer user (pptruser).
+# RUN groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
+#   && mkdir -p /home/pptruser/Downloads \
+#   && mkdir -p /usr/src/app/screenshots/ \
+#   && chown -R pptruser:pptruser /home/pptruser \
+#   && chown -R pptruser:pptruser /usr/src
+
+# # # Run user as non privileged.
+# USER pptruser
+
+CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const util = require('util');
 const URL = require('url');
 const { generateFilename } = require('./lib/helpers');
-const que = require('./lib/queue');
 const { exec } = require('child_process');
 const ms = require('ms');
+const que = require('./lib/queue');
 
 const { saveScreenshot, saveEmptyScreenshot } = require('./lib/save-resize.js');
-const { cleanup, tabsStatus, concurentJobs } = require('./lib/remote-interface.js');
+// const { cleanup, tabsStatus, concurentJobs } = require('./lib/remote-interface.js');
 
 const readFilePromise = util.promisify(fs.readFile);
 const port = process.env.PORT || 3000;
@@ -18,47 +18,42 @@ process.rootPath = __dirname;
 
 const requestHandler = async (request, response) => {
   if (request.url.includes('/healthcheck')) {
-    tabsStatus().then(() => {
-      response.statusCode = 201;
-      response.end();
-    })
-    .catch(() => {
-      response.statusCode = 500;
-      response.end();
-    });
-    return;
+    response.statusCode = 201;
+    response.end();
   }
   if (!request.url.includes('/capture')) {
     response.statusCode = 404;
     response.end('Not Found');
     return;
   }
-  const query = URL.parse(request.url, true).query;
+  const { query } = URL.parse(request.url, true);
   const format = query.format || 'jpeg';
   const isThumbRequest = query.thumb || false;
   const imgUrl = query.url;
   if (!imgUrl || imgUrl === '' || /\.pdf/.test(imgUrl)) {
     response.writeHead(200, {
       'Content-Type': `image/${format}`,
-      'Cache-Control': `public, max-age=${imageCache}`,
+      'Cache-Control': `public, max-age=${imageCache}`
     });
     response.write(emptyFile, 'binary');
     response.end();
     return;
   }
-  const fileName = `${generateFilename(query)}${isThumbRequest ? '_t' : ''}.${isThumbRequest ? 'jpeg' : format}`;
+  const fileName = `${generateFilename(query)}${isThumbRequest ? '_t' : ''}.${
+    isThumbRequest ? 'jpeg' : format
+  }`;
   try {
     const file = await readFilePromise(fileName);
     if (request.method === 'HEAD') {
       response.writeHead(200, {
-        'Cache-Control': 'no-cache, maxage=0',
+        'Cache-Control': 'no-cache, maxage=0'
       });
       response.end();
       return;
     }
     response.writeHead(200, {
       'Content-Type': `image/${format}`,
-      'Cache-Control': `public, max-age=${imageCache}`,
+      'Cache-Control': `public, max-age=${imageCache}`
     });
     response.write(file, 'binary');
     response.end();
@@ -66,23 +61,26 @@ const requestHandler = async (request, response) => {
   } catch (readError) {
     if (request.method === 'HEAD') {
       response.writeHead(201, {
-        'Cache-Control': 'no-cache, maxage=0',
+        'Cache-Control': 'no-cache, maxage=0'
       });
       response.end();
     } else {
       saveScreenshot(query)
-        .then(async (savedFilename) => {
+        .then(async savedFilename => {
           const file = await readFilePromise(savedFilename);
           response.writeHead(200, {
             'Content-Type': `image/${format}`,
-            'Cache-Control': `public, max-age=${imageCache}`,
+            'Cache-Control': `public, max-age=${imageCache}`
           });
           response.write(file, 'binary');
           response.end();
         })
-        .catch((error) => {
+        .catch(async error => {
           console.error(error);
-          response.end('500', error);
+          await saveEmptyScreenshot(query);
+          // response.end('500', error);
+          response.write(emptyFile, 'binary');
+          response.end();
         });
     }
   }
@@ -90,7 +88,7 @@ const requestHandler = async (request, response) => {
 
 const server = http.createServer(requestHandler);
 server.maxConnections = 10000000;
-server.listen(port, (err) => {
+server.listen(port, err => {
   if (err) {
     console.log('Server Error:', err);
   }
@@ -98,50 +96,30 @@ server.listen(port, (err) => {
   console.log(`Server is listening on ${port}`);
 });
 
-// we close all tabs and schedule the cleaning task
-let openTabs = 0;
-let similarTabsCount = 1;
-cleanup().then(async () => {
-  setInterval(async () => {
-    const tabs = await tabsStatus();
-    const currentOpenTabs = tabs.length;
-    console.log(`OPEN_TABS: ${currentOpenTabs}`);
+const cleanDaysTimeout = Number(process.env.FILES_CLEAN_TIMEOUT) || 1;
+const watchQueue = Number(process.env.WATCH_QUEUE) || 0;
 
-    if (currentOpenTabs > 1 && (currentOpenTabs === openTabs)) {
-      similarTabsCount += 1;
-    } else {
-      similarTabsCount = 1;
-    }
-    openTabs = currentOpenTabs;
-    if ((similarTabsCount > 5) || openTabs > (concurentJobs * 2)) {
-      console.log('Too many open tabs!!');
-      await cleanup();
-      openTabs = 0;
-      similarTabsCount = 1;
-    }
-  }, ms('30s'));
-  const cleanDaysTimeout = Number(process.env.FILES_CLEAN_TIMEOUT) || 1;
-  const watchQueue = Number(process.env.WATCH_QUEUE) || 0;
-
-  // Cleaning up the old images every 10 days
-  if (cleanDaysTimeout) {
-    setInterval(() => {
-      exec(`find ${__dirname}/screenshots/ -mtime +${cleanDaysTimeout} -type f -delete`, (err, stdout, stderr) => {
+// Cleaning up the old images every 10 days
+if (cleanDaysTimeout) {
+  setInterval(() => {
+    exec(
+      `find ${__dirname}/screenshots/ -mtime +${cleanDaysTimeout} -type f -delete`,
+      (err, stdout, stderr) => {
         if (err) {
           console.log(`Error Deleting old images: ${stderr}`);
         } else {
           console.log(`10 days old images deleted~ ${stdout}`);
         }
-      });
-    }, ms(`${cleanDaysTimeout}d`));
-  }
+      }
+    );
+  }, ms(`${cleanDaysTimeout}d`));
+}
 
-  // we watch the que and save the files
-  if (watchQueue) {
-    // que.flushDB();
-    que.init();
-    setInterval(() => {
-      que.clearFailed(saveEmptyScreenshot);
-    }, ms('1m'));
-  }
-});
+// we watch the que and save the files
+if (watchQueue) {
+  // que.flushDB();
+  que.init();
+  setInterval(() => {
+    que.clearFailed(saveEmptyScreenshot);
+  }, ms('1m'));
+}
